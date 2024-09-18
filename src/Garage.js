d5a8cf350ccdbcb8b696425b385041aefe17d2e3
parkjaehak <atom8426@naver.com>
2024-09-18 10:20:53 +0900

    Modify: Garage to Garage2

--- src/Garage.js
+++ src/Garage.js
@@ -2,7 +2,7 @@ export default function Garage(props) {
   const cars = props.cars;
   return (
     <>
-      <h1>Garage</h1>
+      <h1>Garage2</h1>
       {cars.length > 0 && <h2>You have {cars.length} cars in your garage.</h2>}
     </>
   );
