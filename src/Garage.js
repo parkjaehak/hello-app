d73318ffe7d20cca987912ea1c8af6ef7b51303f
parkjaehak <atom8426@naver.com>
2024-09-18 09:40:04 +0900

    Init: create hello-app

---
+++ src/Garage.js
@@ -0,0 +1,9 @@
+export default function Garage(props) {
+  const cars = props.cars;
+  return (
+    <>
+      <h1>Garage</h1>
+      {cars.length > 0 && <h2>You have {cars.length} cars in your garage.</h2>}
+    </>
+  );
+}
